b7bbfeff0066dcb4b0299c12ac52427670481c5c
tubelight056 <69566026+tubelight056@users.noreply.github.com>
2021-07-14 18:54:37 +0530

    Revert "initial commit 😎😎"
    This reverts commit 9cfdb8a68fbe54189879c8e8aa1feda87978990f.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-
+import logo from './logo.svg';
 import './App.css';
-import React from 'react'
-import {BrowserRouter as Router ,Route,Switch } from 'react-router-dom'
-import Navbar from "./components/Navbar.js"                                                                                                             
-import Mainpage from './components/Mainpage.js';
+
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar/>
-        <Route path="/" component={Mainpage} />
-        
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+}
 
 export default App;
